051bc3efb0e6000f00c153c1ce79bd60ce875b5b
vinh13 <58327993+vinh13@users.noreply.github.com>
2022-06-11 16:50:49 +0700

    ui

---
+++ Assets/vinh/Script/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeController : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void openSkin()
+    {
+
+    }
+
+	public void closeSkin()
+    {
+
+    }
+
+
+}
